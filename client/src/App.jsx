8717deab710aadc84220363b397da15f64afa0d3
partisi <erolmb@gmail.com>
2022-10-30 15:57:31 -0400

    intro home designed w/ handle view specific

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Row, Col, Typography, Divider } from "antd";
 import axios from "axios";
 import CategoryIntroCard from "./components/CategoryIntroCard";
+import ViewCard from "./components/ViewCard";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBurger,
   faCar,
@@ -23,39 +23,59 @@ function App() {
     });
   }, []);
 
-  return (
-    <div className="app">
-      <Title level={1}>Carbon Footprint Calculator</Title>
-      <Divider orientation="center">
-        Where would you like to calculate your carbon footprint?
-      </Divider>
+  const [viewingCard, setViewingCard] = useState(null);
 
-      <Row justify="center">
-        <CategoryIntroCard
-          title="Housing"
-          icon={<FontAwesomeIcon icon={faHouse} size="4x" />}
-        />
-        <CategoryIntroCard
-          title="Travel"
-          icon={<FontAwesomeIcon icon={faCar} size="4x" />}
-        />
-        <CategoryIntroCard
-          title="Food"
-          icon={<FontAwesomeIcon icon={faBurger} size="4x" />}
-        />
-      </Row>
-      <Row justify="center">
-        <CategoryIntroCard
-          title="Products"
-          icon={<FontAwesomeIcon icon={faShirt} size="4x" />}
-        />
-        <CategoryIntroCard
-          title="Services"
-          icon={<FontAwesomeIcon icon={faSchool} size="4x" />}
-        />
-      </Row>
-    </div>
-  );
+  function handleCardClicked(key) {
+    setViewingCard(key);
+  }
+
+  if (viewingCard === null) {
+    return (
+      <div className="app">
+        <Title level={1}>Carbon Footprint Calculator</Title>
+        <Divider orientation="center">
+          Where would you like to calculate your carbon footprint?
+        </Divider>
+
+        <Row justify="center">
+          <CategoryIntroCard
+            title="Housing"
+            color="red"
+            icon={faHouse}
+            onClick={handleCardClicked}
+          />
+          <CategoryIntroCard
+            title="Travel"
+            color="orange"
+            icon={faCar}
+            onClick={handleCardClicked}
+          />
+          <CategoryIntroCard
+            title="Food"
+            color="green"
+            icon={faBurger}
+            onClick={handleCardClicked}
+          />
+        </Row>
+        <Row justify="center">
+          <CategoryIntroCard
+            title="Products"
+            color="blue"
+            icon={faShirt}
+            onClick={handleCardClicked}
+          />
+          <CategoryIntroCard
+            title="Services"
+            color="purple"
+            icon={faSchool}
+            onClick={handleCardClicked}
+          />
+        </Row>
+      </div>
+    );
+  } else {
+    return <ViewCard key={viewingCard} />;
+  }
 }
 
 export default App;
--- client/src/components/CategoryIntroCard.jsx
+++ client/src/components/CategoryIntroCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Divider } from "antd";
 import Card from "antd/lib/card/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const { Title } = Typography;
 
 /**
  * The initial simple introduction for the categories to select
  * @returns
  */
-const CategoryIntroCard = ({ title, icon }) => {
+const CategoryIntroCard = ({ title, icon, color, onClick }) => {
   return (
-    <Col className="custom-intro-card">
-      <Row justify="center">
-        <Title>{title}</Title>
-      </Row>
-      <Row justify="center" align="middle">{icon}</Row>
+    <Col className="custom-intro-card" style={{ borderColor: color }} onClick={() => onClick(title)}>
+      <FontAwesomeIcon icon={icon} size="3x" color={color} />
+      <Divider orientation="center"></Divider>
+      <Title level={4}>{title}</Title>
     </Col>
   );
 };
